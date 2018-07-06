a4e097ed9c0879eee313e23033252d02656960e7
lsytj0413 <511121939@qq.com>
2018-07-06 10:12:26 +0800

    js/enable es6

--- js/p0001/p0001.test.js
+++ js/p0001/p0001.test.js
@@ -1,7 +1,8 @@
-// import p0001 from './p0001';
+import sum from './p0001';
 
-const sum = require('./p0001.js');
+// const sum = require('./p0001.js');
 
 test('test 1 plus 2 result', () => {
+  console.log(sum);
   expect(sum(1, 2)).toBe(3);
 });
