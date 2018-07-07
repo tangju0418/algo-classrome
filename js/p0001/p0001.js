9f5997b26f9905dfc0cfbd96a324d9e9d9204487
唐菊 <635465547@qq.com>
2018-07-07 07:40:51 -0700

    给定一个整数数组和一个目标值，找出数组中和为目标值的两个数的下标

--- js/p0001/p0001.js
+++ js/p0001/p0001.js
@@ -1,3 +1,15 @@
-export default function sum(a, b) {
-  return a + b;
+export default function findSum(arr, b){
+    let len = arr.length;
+    let index0 = -1;
+    let index1 = -1;
+    arr.forEach((a,index) => {
+        for(let i=index+1; i< len; i++){
+            if(a + arr[i] == b){
+                index0 = index;
+                index1 = i;
+                return;
+            }
+        }
+    });
+    return [index0, index1]
 }
--- js/p0001/p0001.test.js
+++ js/p0001/p0001.test.js
@@ -1,8 +1,8 @@
-import sum from './p0001';
+import findSum from './p0001';
 
-// const sum = require('./p0001.js');
+// const sum = require('./pdemo.js');
 
-test('test 1 plus 2 result', () => {
-  console.log(sum);
-  expect(sum(1, 2)).toBe(3);
+test('test plus two number in array equals another number, return the indexes result', () => {
+  console.log(findSum);
+  expect(findSum([1, 2, 3, 4, 5], 8)).toEqual([2,4]);
 });
