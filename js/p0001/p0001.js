9a8e8d3041284d182cd2cfc68c697060321d2b8e
唐菊 <635465547@qq.com>
2018-07-08 06:04:29 -0700

    update js/p0001

--- js/p0001/p0001.js
+++ js/p0001/p0001.js
@@ -1,10 +1,10 @@
-export default function findSum(arr, b){
-    let len = arr.length;
+export default function findSum(nums, target){
+    let len = nums.length;
     let index0 = -1;
     let index1 = -1;
-    arr.forEach((a,index) => {
+    nums.forEach((a,index) => {
         for(let i=index+1; i< len; i++){
-            if(a + arr[i] == b){
+            if(a + nums[i] == target){
                 index0 = index;
                 index1 = i;
                 return;
