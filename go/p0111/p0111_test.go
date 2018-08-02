30e159d0e3d2c60a1b8fa1f21b848634be177d49
lsytj0413 <511121939@qq.com>
2018-08-02 10:21:14 +0800

    go/p0111

---
+++ go/p0111/p0111_test.go
@@ -0,0 +1,69 @@
+// You may obtain a copy of the License at
+//
+//     https://opensource.org/licenses/MIT
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package p0111
+
+import (
+	"testing"
+
+	"github.com/lsytj0413/algo-classrome/go/comm"
+	"github.com/stretchr/testify/suite"
+)
+
+type record struct {
+	arg1   []int
+	target int
+}
+
+type result struct {
+	arg1   *TreeNode
+	target int
+}
+
+var records = []record{
+	// {
+	// 	arg1:   []int{3, 9, 20, 0xFFFFFFFF, 0xFFFFFFFF, 15, 7},
+	// 	target: 2,
+	// },
+	// {
+	// 	arg1:   []int{1, 2},
+	// 	target: 2,
+	// },
+	{
+		arg1:   []int{1, 2, 3, 4, 5},
+		target: 2,
+	},
+}
+
+var values []result
+
+type p0111TestSuite struct {
+	suite.Suite
+}
+
+func (s *p0111TestSuite) Test() {
+	for _, v := range values {
+		s.Equal(v.target, minDepth(v.arg1))
+	}
+}
+
+func TestP0111TestSuite(t *testing.T) {
+	s := &p0111TestSuite{}
+	suite.Run(t, s)
+}
+
+func init() {
+	for _, v := range records {
+		values = append(values, result{
+			arg1:   comm.Trees(v.arg1),
+			target: v.target,
+		})
+	}
+}
