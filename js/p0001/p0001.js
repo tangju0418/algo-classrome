59eb72796d3dd1501edc8693ad8e4793e7e23ad7
唐菊 <635465547@qq.com>
2018-08-02 05:04:38 -0700

    p0001 改用map方法

--- js/p0001/p0001.js
+++ js/p0001/p0001.js
@@ -2,14 +2,25 @@ export default function findSum(nums, target){
     const len = nums.length;
     let index0 = -1;
     let index1 = -1;
-    nums.forEach((a,index) => {
-        for(let i=index+1; i< len; i++){
-            if(a + nums[i] == target){
-                index0 = index;
-                index1 = i;
-                return;
-            }
+    let s = new Map();
+    for( let i=0;i < nums.length;i++){
+        let a = nums[i]
+        if(s.has(target - a)){
+            index0 = s.get(target - a)
+            index1 = i
+            break;
         }
-    });
+        s.set(a, i)
+        
+    }
+    // nums.forEach((a,index) => {
+    //     for(let i=index+1; i< len; i++){
+    //         if(a + nums[i] == target){
+    //             index0 = index;
+    //             index1 = i;
+    //             return;
+    //         }
+    //     }
+    // });
     return [index0, index1]
 }
