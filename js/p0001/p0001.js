62c485dd24daab56b2e9d76b9883f5bc23dfd06d
唐菊 <635465547@qq.com>
2018-08-02 07:22:45 -0700

    refactor(js): p0001 remove map.has call

--- js/p0001/p0001.js
+++ js/p0001/p0001.js
@@ -2,10 +2,11 @@ export default function findSum(nums, target){
     const len = nums.length;
     const s = new Map();
 
-    for( let i=0;i < nums.length;i++){
+    for( let i=0;i < len;i++){
         let a = nums[i]
-        if(s.has(target - a)){
-            return [s.get(target - a), i]
+        let index = s.get(target - a)
+        if(index != undefined){
+            return [index, i]
         }
         s.set(a, i)
     }
