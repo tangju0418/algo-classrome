e4c5874704f340f7d82af4e0923df4353fb02343
唐菊 <635465547@qq.com>
2018-08-07 06:00:45 -0700

    refactor(js): p0001 conform to eslint

--- js/p0001/p0001.test.js
+++ js/p0001/p0001.test.js
@@ -2,5 +2,5 @@ import findSum from './p0001';
 
 
 test('test plus two number in array equals another number, return the indexes result', () => {
-  expect(findSum([1, 2, 3, 4, 5], 8)).toEqual([2,4]);
+  expect(findSum([1, 2, 3, 4, 5], 8)).toEqual([2, 4]);
 });
