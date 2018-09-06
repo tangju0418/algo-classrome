a9f8d5efecfc4067f56f525b913db71f17ca2fe2
lsytj0413 <511121939@qq.com>
2018-09-06 15:47:50 +0800

    feat(go): p0543 resolve

---
+++ go/p0543/p0543_test.go
@@ -0,0 +1,46 @@
+// You may obtain a copy of the License at
+//
+//     https://opensource.org/licenses/MIT
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package p0543
+
+import (
+	"testing"
+
+	"github.com/lsytj0413/algo-classrome/go/comm"
+	"github.com/stretchr/testify/suite"
+)
+
+type value struct {
+	arg1   []int
+	target int
+}
+
+var values = []value{
+	{
+		arg1:   []int{1, 2, 3, 4, 5},
+		target: 3,
+	},
+}
+
+type p0543TestSuite struct {
+	suite.Suite
+}
+
+func (s *p0543TestSuite) Test() {
+	for _, v := range values {
+		r := diameterOfBinaryTree(comm.Trees(v.arg1))
+		s.Equal(v.target, r)
+	}
+}
+
+func TestP0543TestSuite(t *testing.T) {
+	s := &p0543TestSuite{}
+	suite.Run(t, s)
+}
