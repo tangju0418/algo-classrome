41cda474f525130e3923972b0b9cc42acfad438b
唐菊 <635465547@qq.com>
2018-09-08 22:08:33 +0800

    refactor(js): p0002 eslint  dontn't review "linebreak-style": 0, remove useless code

--- js/p0002/p0002.test.js
+++ js/p0002/p0002.test.js
@@ -5,8 +5,6 @@ import addTwoNumbers from './p0002';
 // 输出：7 -> 0 -> 8
 // 原因：342 + 465 = 807
 test('test plus two list, return the result', () => {
-  console.log('+++++', typeof (expect));
-
   const arr1 = [2, 4, 3];
   const arr2 = [5, 6, 4];
   const res = [7, 0, 8];
