7c34f9897b0921f7d4be72a8bd4ace5d73f28e07
lsytj0413 <511121939@qq.com>
2018-09-28 15:34:57 +0800

    feat(go): p0049 resolve

---
+++ go/p0049/p0049.go
@@ -0,0 +1,43 @@
+// Copyright (c) 2018 soren yang
+//
+// Licensed under the MIT License
+// you may not use this file except in complicance with the License.
+// You may obtain a copy of the License at
+//
+//     https://opensource.org/licenses/MIT
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package p0049
+
+import (
+	"sort"
+	"strings"
+)
+
+func groupAnagrams(strs []string) [][]string {
+	_sort := func(v string) string {
+		arr := strings.Split(v, "")
+		sort.Strings(arr)
+		return strings.Join(arr, "")
+	}
+
+	maps := map[string][]string{}
+	for _, str := range strs {
+		v := _sort(str)
+		maps[v] = append(maps[v], str)
+	}
+
+	ret, w := make([][]string, len(maps)), 0
+	for _, v := range maps {
+		sort.Strings(v)
+		ret[w] = v
+		w++
+	}
+
+	return ret
+}
