0f3b31f342e5837ac1a4610a49f3146a8bf81d0c
唐菊 <635465547@qq.com>
2018-10-01 03:12:28 -0700

    refactor: (js) p0007.test.js

--- js/p0007/p0007.test.js
+++ js/p0007/p0007.test.js
@@ -1,7 +1,22 @@
 import reverse from './p0007';
 
 test('reverse a number', () => {
-  expect(reverse(123)).toEqual(321);
-  expect(reverse(-123)).toEqual(-321);
-  expect(reverse(2147483647)).toEqual(0);
+  const testArr = [
+    {
+      origin: 123,
+      target: 321,
+    },
+    {
+      origin: -123,
+      target: -321,
+    },
+    {
+      origin: 2147483647,
+      target: 0,
+    },
+  ];
+
+  testArr.forEach((el) => {
+    expect(reverse(el.origin)).toEqual(el.target);
+  });
 });
