10cf627eb6c50cfe26e61564c45d020bd17528a5
唐菊 <635465547@qq.com>
2018-10-14 15:04:04 +0800

    refactor: (js) p0014 p0020

--- js/p0014/p0014.js
+++ js/p0014/p0014.js
@@ -6,19 +6,20 @@ export default function longestCommonPrefix(strs) {
   } else if (len === 1) {
     return strs[0];
   }
-  let publicStr = '';
+
   let i = 0;
   const strs0Len = strs[0].length;
   while (i < strs0Len) {
     const substr = strs[0].charAt(i);
     for (let x = 0; x < len; x += 1) {
       const currentChar = strs[x].charAt(i);
-      if (currentChar === '' || currentChar === undefined || currentChar !== substr) {
-        return publicStr;
+      if (currentChar === undefined || currentChar !== substr) {
+        return strs[0].slice(0, i);
       }
     }
-    publicStr += substr;
+
     i += 1;
   }
-  return publicStr;
+
+  return strs[0].slice(0, i);
 }
