459e652ac8c1614577f6e1b46eb88cdc05ec60d0
lsytj0413 <511121939@qq.com>
2018-10-17 14:18:15 +0800

    feat(go): p0215 Kth Largest Element in an Array

---
+++ go/p0215/p0215.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2018 soren yang
+//
+// Licensed under the MIT License
+// you may not use this file except in complicance with the License.
+// You may obtain a copy of the License at
+//
+//     https://opensource.org/licenses/MIT
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package p0215
+
+import (
+	"sort"
+)
+
+// TODO: quick select implement
+func findKthLargest(nums []int, k int) int {
+	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+
+	return nums[k-1]
+}
