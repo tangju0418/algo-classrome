aa5454e94500a5005abeccb2a5aa72e7bfa28b84
唐菊 <635465547@qq.com>
2018-10-20 21:08:55 +0800

    refactor(js): p0459

--- js/p0459/p0459.js
+++ js/p0459/p0459.js
@@ -4,29 +4,30 @@ export default function repeatedSubstringPattern(s) {
   if (len <= 1) {
     return false;
   }
-  let subStr = s[0];
 
   for (let i = 1; i <= len / 2; i += 1) {
-    const subLen = subStr.length;
-    if (len % subLen != 0) {
-      subStr += s[i];
+    if (len % i !== 0) {
       //eslint-disable-next-line
       continue;
     }
 
-    let p = i;
-    for (; p < len;) {
-      const nextStr = s.slice(p, p + subLen);
-      if (subStr === nextStr) {
-        p += subLen;
-      } else {
-        break;
-      }
-    }
-    if (p === len) {
+    if (subStrjoinS(i)) {
       return true;
     }
-    subStr += s[i];
+  }
+
+  // i前的子字符串能否重复组成s
+  function subStrjoinS(i) {
+    const subStr = s.slice(0, i);
+    for (let p = 0; p < len;) {
+      const nextStr = s.slice(p, p + i);
+      if (subStr === nextStr) {
+        p += i;
+      } else {
+        return false;
+      }
+    }
+    return true;
   }
 
   return false;
