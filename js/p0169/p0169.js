1d53b2b2b9494ccde3484a1129c23efbb7a7420e
唐菊 <635465547@qq.com>
2018-11-05 20:41:55 +0800

    refactor(js): p0169

--- js/p0169/p0169.js
+++ js/p0169/p0169.js
@@ -1,17 +1,19 @@
 // majority-element
 export default function majorityElement(nums) {
   const len = nums.length;
-  let num = nums[0];
-  let count = 1;
-  let i = 1;
+  let num = 0;
+  let count = 0;
+  let i = 0;
   while (i < len) {
+    if (count === 0) {
+      num = nums[i];
+      count = 1;
+      i += 1;
+      //eslint-disable-next-line
+      continue;
+    }
     if (nums[i] !== num) {
       count -= 1;
-      if (count === 0) {
-        num = nums[i + 1];
-        count = 1;
-        i += 1;
-      }
     } else {
       count += 1;
     }
