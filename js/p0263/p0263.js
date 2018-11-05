234c1fe18f2e5b4771a5bb805fe8b35157d2eb4a
唐菊 <635465547@qq.com>
2018-11-05 20:49:38 +0800

    refactor(js): p0263

--- js/p0263/p0263.js
+++ js/p0263/p0263.js
@@ -1,9 +1,9 @@
 // only have factors 2,3,5
 /* eslint no-param-reassign: 0 */
 export default function isUgly(num) {
-  //eslint-disable-next-line
-  function factor(num) {
-    const preNum = num;
+  let preNum = Number.NEGATIVE_INFINITY;
+  while (num > 1 && preNum !== num) {
+    preNum = num;
     if (num >= 2 && num % 2 === 0) {
       num /= 2;
     } else if (num >= 3 && num % 3 === 0) {
@@ -11,14 +11,6 @@ export default function isUgly(num) {
     } else if (num >= 5 && num % 5 === 0) {
       num /= 5;
     }
-
-    if (num === preNum || num === 1) {
-      return num;
-    }
-
-    return factor(num);
   }
-
-  const minNum = factor(num);
-  return minNum === 1;
+  return num === 1;
 }
